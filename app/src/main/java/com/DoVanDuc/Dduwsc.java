4685fd6543f7198930fb2ae9b31fb28481008a4c
Dduwsc <duchainaond@gmail.com>
2020-07-18 01:58:36 +0700

    test lần đầu đưa code lên github

---
+++ app/src/main/java/com/DoVanDuc/Dduwsc.java
@@ -0,0 +1,13 @@
+package com.DoVanDuc;
+
+import androidx.annotation.NonNull;
+
+public class Dduwsc extends Double {
+    public Dduwsc(double value) {
+        super(value);
+    }
+
+    public Dduwsc(@NonNull String s) throws NumberFormatException {
+        super(s);
+    }
+}
